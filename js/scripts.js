3c24b21ebc753641130c4ae945c703870eeddeb0
Tyler Stewart <mr.tstew@gmail.com>
2022-04-24 13:05:14 -0700

    add unique IDs for contacts

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// Business Logic for AddressBook ---------
+function AddressBook() {
+  this.contacts = {};
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function (contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+};
+
+AddressBook.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function () {
+  return this.firstName + " " + this.lastName;
+};
---
+++ objects.js
@@ -0,0 +1,9 @@
+let pdx = { name: "Portland" };
+let sfo = { name: "San Francisco" };
+let sea = { name: "Seattle" };
+let cso = { name: "Aktau" };
+let dzn = { name: "Zhezkazgan" };
+
+let usa = { name: "United States of America" };
+let kazakhstan = { name: "Kazakhstan" };
+let uruguay = { name: "Uruguay" };
